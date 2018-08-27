df1003a988f9f4281ad0d827c837dbb6281392cb
alexaver <averbuch.alex7@gmail.com>
2018-08-27 20:08:42 +0300

    add debug toolbar and prettyurl

--- config/web.php
+++ config/web.php
@@ -1,5 +1,19 @@
 <?php
     return [
         'id' => 'school-web',
-        'basePath' => realpath( __DIR__ . '/../')
+        'basePath' => realpath( __DIR__ . '/../'),
+        'language' => 'ru',
+        'bootstrap' => ['debug'],
+        'components' => [
+            'urlManager' => [
+                'enablePrettyUrl' => true,
+                'showScriptName' => false
+                ],
+            'request' => [
+                'cookieValidationKey' => 'ewrewrwererere'
+                ]
+            ],
+        'modules' => [
+            'debug' => 'yii\debug\Module'
+            ]
        ];
